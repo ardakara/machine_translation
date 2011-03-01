52aafade5550afd05925cdb2c5dd17f920f31b7c
Jason Ma <majj@stanford.edu>
2011-02-28 21:31:47 -0800

    Starting some code.  Very basic stuff -- reads the English-Turkish dictionary into a (Python) dictionary.  Includes some rudimentary word-by-word-translation.

---
+++ translation.py
@@ -0,0 +1,32 @@
+# CS 124 Assignment 7
+# Machine Translation
+# 
+# Arda Kara
+# Jason Ma
+
+dictionary = dict()
+
+def readDictionary(file):
+  dictFile = open(file, 'r')
+  lines = dictFile.read().split('\n')
+  for line in lines:
+    wordpair = line.split('#')
+    if len(wordpair) > 1:
+      dictionary[wordpair[0]] = wordpair[1]
+  dictFile.close()
+
+def readFile(file):
+  sourceFile = open(file, 'r')
+  return sourceFile.read()
+  sourceFile.close()
+      
+def translateWords(string):
+  words = string.split(' ')
+  result = ''
+  for word in words:
+    result += dictionary[word] + ' '
+  return result
+    
+readDictionary('dictionary.txt')
+source = readFile('source.txt')
+print translateWords(source)
