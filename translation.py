886b19c3c1156fbc2ec725e768bcaa939f3688cc
Jason Ma <majj@stanford.edu>
2011-03-03 17:33:59 -0800

    Normalizes the input; basic translation achieved.
    Processes the dictionary and input string and does normalization.
    All that's left is re-write rules.

--- translation.py
+++ translation.py
@@ -12,6 +12,7 @@ def readDictionary(file):
   dictFile = open(file, 'r')
   lines = dictFile.read().split('\n')
   for line in lines:
+    line = normalize(line)
     wordpair = line.split('#')
     if len(wordpair) > 1:
       dictionary[wordpair[0]] = wordpair[1]
@@ -24,20 +25,35 @@ def readFile(file):
   return fileText
       
 def translateWords(string):
-  words = string.split(' ')
+  tokens = string.split()
   result = ''
-  for word in words:
-    result += dictionary[word] + ' '
+  for token in tokens:
+    if len(token) == 0:
+      continue
+    # Sometimes, words will have punctuation attached.  Usually at the end.
+    # In our case, we only have commas and periods to deal with, and never both.
+    
+    word = token
+    punct = ''
+    if token[-1] == '.' or token[-1] == ',':
+      word = token[0:-1]
+      punct = token[-1]
+    
+    if word in dictionary:
+      translated = dictionary[word]
+      result += dictionary[word] + punct + ' '
+    else:
+      print 'Error: ' + word + ' not in dictionary.  Skipping word...'
   return result
 
-# Convert all letters to lower case and remove punct
+# Convert all letters to lower case
 def normalize(source):
   normalize = ''
   for ch in source:
     upperIdx = string.ascii_uppercase.find(ch)
     if upperIdx >= 0:
       normalize += string.ascii_lowercase[upperIdx]
-    else
+    else:
       normalize += ch
   return normalize
       
@@ -48,6 +64,5 @@ source = readFile('source.txt')
 normalized = normalize(source)
 print translateWords(normalized)
 
-# need to restore punctuation somehow
-# perhaps also restore capitalization?
+# should we also restore capitalization?
 
